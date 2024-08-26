800d7667410da8f786d385655046762ad7e5eb83
gerald <geraldbaalham@live.co.uk>
2024-08-26 10:55:52 +0100

    left sidebar routes created

---
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -0,0 +1,8 @@
+import { Podcast } from "lucide-react";
+import React from "react";
+
+const PodcastDetails = ({ params }: { params: { podcastId: string } }) => {
+  return <p className="text-white-1">Podcast Details for {params.podcastId}</p>;
+};
+
+export default PodcastDetails;
